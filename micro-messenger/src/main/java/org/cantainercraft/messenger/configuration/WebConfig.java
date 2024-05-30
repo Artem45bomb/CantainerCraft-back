d87f2c6b973ce24dca23173a8cb0d3652065ddf4
Artem45bomb <artemsenkevich@gmail.com>
2024-05-30 14:54:37 +0300

    websocket router create sesssion

--- micro-messenger/src/main/java/org/cantainercraft/messenger/configuration/WebConfig.java
+++ micro-messenger/src/main/java/org/cantainercraft/messenger/configuration/WebConfig.java
@@ -1,14 +1,18 @@
 package org.cantainercraft.messenger.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebConfig implements WebSocketMessageBrokerConfigurer, WebSocketConfigurer {
+
+    private final WebSocketFileHandler webSocketFileHandler;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -16,8 +20,16 @@ public class WebConfig implements WebSocketMessageBrokerConfigurer {
         config.setApplicationDestinationPrefixes("/app");
     }
 
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/gs-guide-websocket").setAllowedOriginPatterns("*");
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketFileHandler,"/file-handler")
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                .setAllowedOrigins("*");
     }
 }
